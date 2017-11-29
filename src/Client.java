3dba1f6fdc4c7bb4edf4ed01626e06c2aa8a0b2b
fruneauxm <34035678+fruneauxm@users.noreply.github.com>
2017-11-29 14:34:00 -0500

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -4,8 +4,6 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
-import java.util.concurrent.CountDownLatch;
-import javafx.application.Platform;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.*;
@@ -13,19 +11,23 @@ import javafx.scene.input.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.animation.*;
 import javafx.concurrent.*;
 
 public class Client extends Application {
-	
-	static String IP = "192.168.1.11"; // This should be your computers IP!!!!!!!!!!!
-	static int port = 2224; // Port should match servers!!!!!!!!!!!
+
+	static String IP = "10.200.53.133"; // This should be your computers IP!!!!!!!!!!!
+	static int port = 1114; // Port should match servers!!!!!!!!!!!
 	private static final int PADDLE_MOVEMENT = 10;
 	DatagramSocket socket;
 	InetAddress IPAddress;
 	static Client client;
 	Rectangle Player1, Player2;
+	static double p1y = 50, p2y = 50;
+	boolean on = true;
 
 	public Client() {
+		
 	}
 
 	public void createAndListenSocket(String address) {
@@ -40,84 +42,70 @@ public class Client extends Application {
 	}
 
 	public static void main(String[] args) {
-		client = new Client();
-		client.createAndListenSocket(IP);
 		launch(args); // invokes JavaFX start method
 	}
 
-	@Override 
-  public void start(Stage stage) throws Exception {
-	//final Circle Ball = new Circle();
-	
-	int p1y = 50;
-	String a = "SetP1Y:" + 50;
-	client.sendMessage(a); // networking shtuff
+	@Override
+	public void start(Stage stage) throws Exception {
+		// final Circle Ball = new Circle();
 
-	Player1 = createRectangle(p1y,200);
-	Player2 = createRectangle(550,200);
-    final Group group = new Group(Player1, Player2);
-    final Scene scene = new Scene(group, 600, 400, Color.BLACK);
-    
-    sendMovesOnKeyPress(scene, Player1, Player2); // key event sends server moves
-   
-    stage.setScene(scene);
-	  
-	  new AnimationTimer()
+		Player1 = createRectangle(50, 200);
+		Player2 = createRectangle(550, 200);
+		final Group group = new Group(Player1, Player2);
+		final Scene scene = new Scene(group, 600, 400, Color.BLACK);
+
+		sendMovesOnKeyPress(scene, Player1, Player2); // key event sends server moves
+
+		stage.setScene(scene);
+		
+		client = new Client();
+		client.createAndListenSocket(IP);
+		
+		Service<Void> service = new Service<Void>() {
+			@Override
+			protected Task<Void> createTask() {
+				return new Task<Void>() {
+					@Override
+					protected Void call() throws Exception {
+						while(on) {
+						Thread.sleep(5000);
+						String[] check = client.get();
+						p1y = Double.parseDouble(check[0]);
+						p2y = Double.parseDouble(check[1]);
+						}
+						return null;
+					}
+				};
+			}
+		};
+		service.start();	
+		
+		new AnimationTimer()
         {
             public void handle(long currentNanoTime)
             {
-            	
-            	String[] check = null;
-				try {
-					check = client.get();
-					Player1.setY(Double.parseDouble(check[0]));
-	        			Player2.setY(Double.parseDouble(check[1]));
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+					Player1.setY(p1y);
+	        			Player2.setY(p2y);
             }
         }.start(); 
-	  
-    stage.show();
-
-	  // not really sure about this
-	  // purpose is to have a thread running in backround talking to server and updating gui
-    // this is not working yet
-    // trying to pull position info from server and update graphics
-//	Service<Void> service = new Service<Void>() {
-//			@Override
-//			protected Task<Void> createTask() {
-//				return new Task<Void>() {
-//					@Override
-//					protected Void call() throws Exception {
-//						client.sendMessage("get:");
-//						byte[] incomingData = new byte[1024];
-//						DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
-//						socket.setSoTimeout(5000);
-//						socket.receive(incomingPacket);
-//						String response = new String(incomingPacket.getData());
-//						String[] check = response.split(":");
-//						final CountDownLatch latch = new CountDownLatch(1);
-//						Platform.runLater(new Runnable() { // need to have to make changes to gui inside thread i think
-//							@Override
-//							public void run() {
-//								try {
-//									Player1.setY(Double.parseDouble(check[0]));
-//									Player2.setY(Double.parseDouble(check[1]));
-//								} finally {
-//									latch.countDown();
-//								}
-//							}
-//						});
-//						latch.await();
-//						return null;
-//					}
-//				};
-//			}
-//		};
-//		service.start();	
 		
+		stage.show();
+
+	}
+	
+	private String[] get() throws IOException {
+		// TODO Auto-generated method stub
+		socket.setSoTimeout(5000);
+		byte[] data = "get:".getBytes(); // convert to bytes
+		DatagramPacket sendPacket = new DatagramPacket(data, data.length, IPAddress, port); // convert to packet
+		socket.send(sendPacket); // send the packet
+		byte[] incomingData = new byte[1024];
+		DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
+		socket.receive(incomingPacket);
+		String response = new String(incomingPacket.getData());
+		System.out.println("From Server:" + response);
+		String[] check = response.split(":");
+		return check;
 	}
 
 	private Rectangle createRectangle(int x, int y) {
@@ -129,15 +117,15 @@ public class Client extends Application {
 	}
 
 	public void sendMessage(String message) { // send the String message
-			try {
-				socket.setSoTimeout(5000);
-				byte[] data = message.getBytes(); // convert to bytes
-				DatagramPacket sendPacket = new DatagramPacket(data, data.length, IPAddress, port); // convert to packet
-				socket.send(sendPacket); // send the packet 
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		try {
+			socket.setSoTimeout(5000);
+			byte[] data = message.getBytes(); // convert to bytes
+			DatagramPacket sendPacket = new DatagramPacket(data, data.length, IPAddress, port); // convert to packet
+			socket.send(sendPacket); // send the packet
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	private void sendMovesOnKeyPress(Scene scene, final Rectangle rect, Rectangle rect1) {
@@ -146,14 +134,15 @@ public class Client extends Application {
 			public void handle(KeyEvent event) {
 				switch (event.getCode()) {
 				case UP:
-					client.sendMessage("p1plus:10");
+					client.sendMessage("p1plus");
 					break;
 				// rect.setY(rect.getY() - KEYBOARD_MOVEMENT_DELTA); break;
 				case DOWN:
-					client.sendMessage("p1minus:10");
+					client.sendMessage("p1minus");
 					break;
-				case ESCAPE:
+				case ESCAPE: 
 					System.exit(0);
+					break;
 					// rect.setY(rect.getY() + KEYBOARD_MOVEMENT_DELTA); break;
 				case U:
 					rect1.setY(rect1.getY() - PADDLE_MOVEMENT);
@@ -167,22 +156,6 @@ public class Client extends Application {
 			}
 		});
 	}
-	
-//	public void update() throws IOException, InterruptedException {
-//	client.sendMessage("get:");
-//	byte[] incomingData = new byte[1024];
-//	DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
-//	socket.setSoTimeout(5000);
-//	socket.receive(incomingPacket);
-//	String response = new String(incomingPacket.getData());
-//	String[] check = response.split(":");
-//	Platform.runLater(new Runnable() {
-//		@Override
-//		public void run() {
-//			Player1.setY(Double.parseDouble(check[0]));
-//			Player2.setY(Double.parseDouble(check[1]));
-//		}
-//	});
-//}
-	
 }
+
+
